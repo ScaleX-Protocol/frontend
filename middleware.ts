a6f8a8a4333bae9ad6791b607e65aac01d25a647
AdityaWisnuu <adtywisnuwardana003@gmail.com>
2025-11-12 21:05:18 +0700

    chore: initiate biome dependency

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const waitlistMode = process.env.NEXT_PUBLIC_WAITLIST_MODE === 'true';
