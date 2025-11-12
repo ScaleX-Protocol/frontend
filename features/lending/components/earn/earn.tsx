35ad6f9855099a2902ecd4119dde7482028be5d0
AdityaWisnuu <adtywisnuwardana003@gmail.com>
2025-11-13 06:29:54 +0700

    feat: add lending layout

---
+++ features/lending/components/earn/earn.tsx
@@ -0,0 +1,5 @@
+export default function Earn() {
+    return (
+        <div className="w-full h-full bg-[#2C2C2C] rounded-md"></div>
+    )
+}
---
+++ features/lending/components/lending.tsx
@@ -0,0 +1,11 @@
+import Borrow from "./borrow/borrow";
+import Earn from "./earn/earn";
+
+export default function Lending() {
+    return (
+        <div className="w-full bg-[#1A1A1A] flex-1 rounded-t-3xl p-4 flex flex-row gap-4">
+            <Earn />
+            <Borrow />
+        </div>
+    )
+}
