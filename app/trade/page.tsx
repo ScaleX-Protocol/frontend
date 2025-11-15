1948e987a9bb50355bb9510d274d2b16b38677b7
AdityaWisnuu <adtywisnuwardana003@gmail.com>
2025-11-16 00:25:50 +0700

    feat: improve button to have basic function login and logout

--- app/trade/page.tsx
+++ app/trade/page.tsx
@@ -8,14 +8,22 @@ import { useWalletState } from '@/hooks/useWalletState';
 import Trade from '@/features/trade/components/trade';
 import Lending from '@/features/lending/components/lending';
 import Faucet from '@/features/faucet/components/faucet';
+import { usePrivy } from '@privy-io/react-auth';
 
 export default function TradePage() {
   const [activeTab, setActiveTab] = useState<'Spot' | 'Lending' | 'Faucet'>('Spot');
+
+  const { authenticated, login, logout } = usePrivy();
 
   const wallet = useWalletState();
 
   const handleLogin = () => {
     wallet.validateAllChains();
+    login();
+  };
+
+  const handleLogOut = () => {
+    logout();
   };
 
   return (
@@ -42,37 +50,50 @@ export default function TradePage() {
           <div className="flex flex-row gap-2">
             <button
               type="button"
-              className={`flex gap-2 py-2 px-3 font-medium rounded-md cursor-pointer ${activeTab === 'Spot' ? 'border-b-2 border-[#F06718]/70' : 'text-[#E0E0E0]/70'
-                }`}
+              className={`flex gap-2 py-2 px-3 font-medium rounded-md cursor-pointer ${
+                activeTab === 'Spot' ? 'border-b-2 border-[#F06718]/70' : 'text-[#E0E0E0]/70'
+              }`}
               onClick={() => setActiveTab('Spot')}
             >
               <ChartCandlestick strokeWidth={1.5} /> Spot
             </button>
             <button
               type="button"
-              className={`flex gap-2 py-2 px-3 font-medium rounded-md cursor-pointer ${activeTab === 'Lending' ? 'border-b-2 border-[#F06718]/70' : 'text-[#E0E0E0]/70'
-                }`}
+              className={`flex gap-2 py-2 px-3 font-medium rounded-md cursor-pointer ${
+                activeTab === 'Lending' ? 'border-b-2 border-[#F06718]/70' : 'text-[#E0E0E0]/70'
+              }`}
               onClick={() => setActiveTab('Lending')}
             >
               <Euro strokeWidth={1.5} /> Lending
             </button>
             <button
               type="button"
-              className={`flex gap-2 py-2 px-3 font-medium rounded-md cursor-pointer ${activeTab === 'Faucet' ? 'border-b-2 border-[#F06718]/70' : 'text-[#E0E0E0]/70'
-                }`}
+              className={`flex gap-2 py-2 px-3 font-medium rounded-md cursor-pointer ${
+                activeTab === 'Faucet' ? 'border-b-2 border-[#F06718]/70' : 'text-[#E0E0E0]/70'
+              }`}
               onClick={() => setActiveTab('Faucet')}
             >
               <Droplet strokeWidth={1.5} /> Faucet
             </button>
           </div>
         </div>
-        <button
-          type="button"
-          onClick={handleLogin}
-          className="py-2 px-4 bg-[#F06718] rounded-md font-medium cursor-pointer hover:bg-[#f0782a] transition-colors duration-200 flex items-center gap-2"
-        >
-          Connect
-        </button>
+        {authenticated ? (
+          <button
+            type="button"
+            onClick={handleLogOut}
+            className="py-2 px-4 bg-[#F06718] rounded-md font-medium cursor-pointer hover:bg-[#f0782a] transition-colors duration-200 flex items-center gap-2"
+          >
+            Log Out
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={handleLogin}
+            className="py-2 px-4 bg-[#F06718] rounded-md font-medium cursor-pointer hover:bg-[#f0782a] transition-colors duration-200 flex items-center gap-2"
+          >
+            Connect
+          </button>
+        )}
       </div>
 
       {activeTab === 'Spot' && <Trade />}
