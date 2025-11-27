e5880a3e64b42f43e778b23c346a24539d143db8
AdityaWisnuu <adtywisnuwardana003@gmail.com>
2025-11-28 00:26:52 +0700

    feat: update env example and add condition url based env

--- configs/endpoints.ts
+++ configs/endpoints.ts
@@ -1,11 +1,13 @@
 export interface EndpointConfig {
-    api: string;
-    indexer: string;
-    websocket: string;
+  api: string;
+  indexer: string;
+  websocket: string;
 }
 
+const isLocalBackend = process.env.NEXT_PUBLIC_BACKEND_ENV === 'local';
+
 export const Endpoints: EndpointConfig = {
-    api: 'https://base-sepolia-api.scalex.money/api',
-    indexer: 'https://base-sepolia-indexer.scalex.money/api',
-    websocket: 'wss://base-sepolia-websocket.scalex.money'
-}
+  api: isLocalBackend ? 'http://localhost:3000/api' : 'https://base-sepolia-api.scalex.money/api',
+  indexer: isLocalBackend ? 'http://localhost:3000/indexer' : 'https://base-sepolia-indexer.scalex.money/api',
+  websocket: isLocalBackend ? 'ws://localhost:8080' : 'wss://base-sepolia-websocket.scalex.money',
+};
