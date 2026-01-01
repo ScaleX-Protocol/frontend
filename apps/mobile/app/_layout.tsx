882865c71070522b866feb253259ee7e13ef6b49
renakaagusta <renaka.agusta@rollingglory.com>
2026-01-01 18:45:50 +0700

    fix: react native configuration

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Providers } from '../providers';
+import '../global.css';
 
 export default function RootLayout() {
   return (
